da1710b734947930d4a33cd16915c90ba74cc162
Vulcanowo <projectvulcano42@gmail.com>
2021-01-02 04:43:36 -0500

    Improved visuals

--- buttons.js
+++
@@ -1,6 +0,0 @@
-function openLink(url) {
-    window.open(url);
-}
-function download() {
-    alert("There is currently no public version of DF Pocket Dev.\nPlease check back later!");
-}
---
+++ script.js
@@ -0,0 +1,12 @@
+function openLink(url) {
+    window.open(url);
+}
+//Downloads the Latest Version of APK or Mod
+function download() {
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        // Downloads APK if on mobile
+      }else{
+        // Downloads mod if on computer
+      }
+    alert("There is currently no public version of DF Pocket Dev.\nPlease check back later!");
+}
