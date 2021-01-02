31c3324f273aa8f73191c83824ce03b4dffe8b7b
Vulcanowo <projectvulcano42@gmail.com>
2021-01-02 17:32:26 -0500

    Added an about page

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ function randomBackground() {
   var node = "url('backgrounds/node" + (Math.floor(Math.random() * Math.floor(4))+1) + ".png')";
   document.body.style.backgroundImage = node;
 }
+function changePage(page) {
+  url = page + ".html";
+  location.href = url;
+}
+
+function aboutLoad() {
+  randomBackground();
+  profile('2ee60a1d-8913-4d8d-8022-72fa0738d0e1', "Designed logo, helped make stylistic descisions.")
+  profile('f33f0e36-31d9-47cb-a4a6-1d30e5816232', "Lead Developer of DFPocketDev.");
+}
+async function profile(uuid, bio) {
+  username = await getUsername(uuid);
+  console.log(username);
+  var profile =
+  "<div class=\"profile\"><div class=\"card\"><img class=\"profile\" src=\"https://mc-heads.net/head/" + uuid + "\"><p class=\"profile\">" + username + "<p></div><p class=\"bio\">" + bio + "<p></div><hr>";
+  document.body.innerHTML = document.body.innerHTML + profile;
+}
+function getUsername(uuid) {
+  return fetch('https://mojang-api.vercel.app/v1/names?uuid=' + uuid)
+  .then(response => response.json())
+  .then(json => {
+      return json[json.length - 1].name;
+  });
+}
