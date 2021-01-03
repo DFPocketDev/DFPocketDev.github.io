021aea8eed497f47a69766581a93545770b6300f
Lachie-Source <62362511+Lachie-Source@users.noreply.github.com>
2021-01-03 17:00:48 +1100

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,25 @@ function openLink(url) {
 }
 //Downloads the Latest Version of APK or Mod
 function download() {
-    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-        // Downloads APK if on mobile
-      }else{
-        // Downloads mod if on computer
-      }
-    alert("There is currently no public version of DF Pocket Dev.\nPlease check back later!");
-    }
+  if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    // Downloads APK if on mobile
+  } else {
+    // Downloads mod if on computer
+  }
+  alert(
+    "There is currently no public version of DF Pocket Dev.\nPlease check back later!"
+  );
+}
 function randomBackground() {
   //Picks a random node background, then sets the background image
-  var node = "url('backgrounds/node" + (Math.floor(Math.random() * Math.floor(4))+1) + ".png')";
+  var node =
+    "url('backgrounds/node" +
+    (Math.floor(Math.random() * Math.floor(4)) + 1) +
+    ".png')";
   document.body.style.backgroundImage = node;
 }
 function changePage(page) {
@@ -22,27 +31,42 @@ function changePage(page) {
 
 function aboutLoad() {
   randomBackground();
-  profile('f33f0e36-31d9-47cb-a4a6-1d30e5816232', "Lead Developer").then(function() {
-    profile('2ee931a2-3541-4f7b-95f2-7f15a039e119', "Developer").then(function() {
-      profile('f4f31774-ec2d-4e1a-8aef-f2906509d927', "Developer").then(function() {
-        profile('2ee60a1d-8913-4d8d-8022-72fa0738d0e1', "Helped design logo and make stylistic descisions");
-      });
-    });
-  });
+  profile("f33f0e36-31d9-47cb-a4a6-1d30e5816232", "Lead Developer").then(
+    function () {
+      profile("2ee931a2-3541-4f7b-95f2-7f15a039e119", "Developer").then(
+        function () {
+          profile("f4f31774-ec2d-4e1a-8aef-f2906509d927", "Developer").then(
+            function () {
+              profile(
+                "2ee60a1d-8913-4d8d-8022-72fa0738d0e1",
+                "Helped design logo and make stylistic descisions"
+              );
+            }
+          );
+        }
+      );
+    }
+  );
 }
 async function profile(uuid, bio) {
   username = await getUsername(uuid);
   console.log(username);
   var profile =
-  "<div class=\"profile\"><div class=\"card\"><img class=\"profile\" src=\"https://mc-heads.net/head/" + uuid + "\"><p class=\"profile\">" + username + "<p></div><p class=\"bio\">" + bio + "<p></div><hr>";
+    '<div class="profile"><div class="card"><img class="profile" src="https://mc-heads.net/head/' +
+    uuid +
+    '"><p class="profile">' +
+    username +
+    '<p></div><p class="bio">' +
+    bio +
+    "<p></div><hr>";
   document.body.innerHTML = document.body.innerHTML + profile;
 }
 function getUsername(uuid) {
-  return fetch('https://mojang-api.vercel.app/v1/names?uuid=' + uuid)
-  .then(response => response.json())
-  .then(json => {
+  return fetch("https://mojang-api.vercel.app/v1/names?uuid=" + uuid)
+    .then((response) => response.json())
+    .then((json) => {
       return json[json.length - 1].name;
-  });
+    });
 }
 function setBackground(node) {
   var bg = "url('backgrounds/node" + node + ".png')";
